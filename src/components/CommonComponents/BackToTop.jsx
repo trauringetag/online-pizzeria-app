adad03e3e779bb55d3c52b5bc334461e779f2507
Kidd Kaine <humans.vlone@gmail.com>
2022-12-23 15:57:30 +0500

    0.13 MUI BackToTop.jsx, работа с адаптивной вёрсткой

---
+++ src/components/CommonComponents/BackToTop.jsx
@@ -0,0 +1,46 @@
+import PropTypes from 'prop-types';
+import useScrollTrigger from '@mui/material/useScrollTrigger';
+import Box from '@mui/material/Box';
+import Fab from '@mui/material/Fab';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import Fade from '@mui/material/Fade';
+
+function ScrollTop (props) {
+    const { children, window } = props;
+
+    const trigger = useScrollTrigger({
+        target: window ? window() : undefined,
+        disableHysteresis: true,
+        threshold: 100,
+    });
+
+    const handleClick = (event) => {
+        const anchor = (event.target.ownerDocument || document).querySelector('#wrapper');
+        if (anchor) anchor.scrollIntoView({block: 'start'});
+    };
+
+    return (
+        <Fade in={trigger}>
+            <Box
+                onClick={ handleClick }
+                role={'presentation'}
+                sx={{ position: 'fixed', bottom: 25, right: 25 }}
+            >
+                { children }
+            </Box>
+        </Fade>
+    );
+}
+
+ScrollTop.propTypes = {
+    children: PropTypes.element.isRequired,
+    window: PropTypes.func,
+};
+
+export const BackToTop = props => (
+    <ScrollTop {...props}>
+        <Fab size={'small'}>
+            <KeyboardArrowUpIcon />
+        </Fab>
+    </ScrollTop>
+);
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,14 @@
 import classes from './Layout.module.scss';
 import Header from "./Header/Header";
 import { Outlet } from "react-router-dom";
+import { BackToTop } from "./CommonComponents/BackToTop";
 
 const Layout = () => (
-    <div className={classes.wrapper}>
+    <div className={classes.wrapper} id={'wrapper'}>
         <section className={classes.container}>
             <Header />
             <Outlet />
+            <BackToTop />
         </section>
     </div>
 );
