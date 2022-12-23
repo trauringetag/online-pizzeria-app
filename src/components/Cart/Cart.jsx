793e39b75c523565b28942b9ceb836febd21acf0
Kidd Kaine <humans.vlone@gmail.com>
2022-12-23 19:26:25 +0500

    0.15 Перенос данных на mockapi, работа с axios, useEffect

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,6 @@
+import classes from './Cart.module.scss';
+
+const Cart = () => (
+    <main className={classes.wrapper}></main>
+);
+export default Cart;
--- src/components/ProductList/Pool/Pool.jsx
+++ src/components/ProductList/Pool/Pool.jsx
@@ -1,9 +1,20 @@
 import classes from './Pool.module.scss';
-import Element from "./Element/Element";
-import database from '../../../assets/database.json';
+import Element from './Element/Element';
+import { useState, useEffect } from "react";
+import axios from 'axios';
 
 const Pool = () => {
-        const displayDatabase = database.map(item => <Element key={item.id} {...item}/>);
-        return <main className={classes.pool}>{ displayDatabase }</main>;
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        axios.get('https://63a5adca318b23efa79a90a6.mockapi.io/pizzas')
+        .then(response => {
+            setItems(response.data);
+        });
+    }, []);
+
+    const displayItems = items.map(item => <Element key={item.id} {...item} />);
+
+    return <main className={classes.wrapper}>{ displayItems }</main>;
 };
 export default Pool;
